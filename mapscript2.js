0936ddc705d41fa7d87c29746bf73d48752b3061
Amandeep Kaur Sidhu <742135@student.tor.loc>
2019-04-11 11:38:43 -0400

    commit

--- mapscript2.js
+++ mapscript2.js
@@ -4,6 +4,6 @@ function initialize(){
         zoom:12,
         center:{lat:40.758904, lng:-73.985128}
     };
-        map = new google.maps.Map(document.getElementById('map3'),mapOption3);
+        map = new google.maps.Map(document.getElementById('map'),mapOptions);
 }
 google.maps.event.addDomListener(window,'load',initialize);
