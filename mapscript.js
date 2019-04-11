5c74339e820e09e7e97e7d4fb3f02bd903f8bbac
Amandeep Kaur Sidhu <742135@student.tor.loc>
2019-04-11 11:09:23 -0400

    commit

---
+++ mapscript.js
@@ -0,0 +1,19 @@
+var mapcanvas1, mapcanvas2, mapcanvas3;
+function initialize(){
+    var mapOption1 = {
+        zoom:2,
+        center:{lat:40.758904, lng:-73.985128}
+    };
+    var mapOption2 = {
+        zoom:10,
+        center:{lat:40.758904, lng:-73.985128}
+    };
+    var mapOption3 = {
+        zoom:18,
+        center:{lat:40.758904, lng:-73.985128}
+    };
+    mapcanvas1 = new google.maps.Map(document.getElementById('map1'),mapOption1);
+    mapcanvas2 = new google.maps.Map(document.getElementById('map2'),mapOption2);
+    mapcanvas3 = new google.maps.Map(document.getElementById('map3'),mapOption3);
+}
+google.maps.event.addDomListener(window,'load',initialize);
